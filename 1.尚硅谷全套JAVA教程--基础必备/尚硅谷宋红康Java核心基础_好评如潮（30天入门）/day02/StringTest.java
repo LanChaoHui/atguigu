c1be3863958d948e15bf21ea558817c444396524
panhu <14245154722@qq.com>
2021-10-31 13:56:44 +0800

    059.尚硅谷_Java基本语法-两个关于String的练习题

--- 1.尚硅谷全套JAVA教程--基础必备/尚硅谷宋红康Java核心基础_好评如潮（30天入门）/day02/StringTest.java
+++ 1.尚硅谷全套JAVA教程--基础必备/尚硅谷宋红康Java核心基础_好评如潮（30天入门）/day02/StringTest.java
@@ -5,24 +5,40 @@
 	3. String 可以和8中基本数据变量做运算，且运算只能是连接运算：+
 	4. 运算的结构仍然是String类型
 */
-class StringTest 
-{
-	public static void main(String[] args) 
-	{
-		String s1="Hello World!";
-		System.out.println(s1);
-		
-		String s2="a";
-		String s3="";
-		// char 类型里面必须放一个空格
-		// char c=''; 编译不通过
-		// ************************************
-		int number=1001;
-		String numberStr="学号：";
-		String info=numberStr+number;
-		System.out.println(info);
-		boolean b1=true;
-		String info1=info+b1;//+ ：连接运算
-		System.out.println(info1);
-	}
+class StringTest {
+    public static void main(String[] args) {
+        String s1="Hello World!";
+        System.out.println(s1);
+
+        String s2="a";
+        String s3="";
+        // char 类型里面必须放一个空格
+        // char c=''; 编译不通过
+        // ************************************
+        int number=1001;
+        String numberStr="学号：";
+        String info=numberStr+number;
+        System.out.println(info);
+        boolean b1=true;
+        String info1=info+b1;//+ ：连接运算
+        System.out.println(info1);
+        // ************************************
+        // 练习1
+        char c='a';// a:97  A:65
+        int num=10;
+        String str="hello";
+        System.out.println(c+num+str);// 107hello
+        System.out.println(c+str+num);//ahelloa
+        System.out.println(c+(num+str));//a10hello
+        System.out.println((c+num)+str);//107hello
+        System.out.println(str+num+c); //hello10a
+        // 练习2
+        //*	*
+        System.out.println("*	*");//*	*
+        System.out.println('*'+'\t'+'*');// 93
+		System.out.println('*'+"\t"+'*');// *	*		
+		System.out.println('*'+'\t'+"*");//51*
+		System.out.println('*'+('\t'+"*"));//*	*
+    }
+
 }
