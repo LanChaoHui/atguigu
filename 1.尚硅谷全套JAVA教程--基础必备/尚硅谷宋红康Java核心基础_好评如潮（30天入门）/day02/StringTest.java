2ad9e4040081b956714d71f86586c060e372abc4
panhu <14245154722@qq.com>
2021-10-31 14:14:47 +0800

    060.尚硅谷_Java基本语法-String使用的强化说明

--- 1.尚硅谷全套JAVA教程--基础必备/尚硅谷宋红康Java核心基础_好评如潮（30天入门）/day02/StringTest.java
+++ 1.尚硅谷全套JAVA教程--基础必备/尚硅谷宋红康Java核心基础_好评如潮（30天入门）/day02/StringTest.java
@@ -38,7 +38,18 @@ class StringTest {
         System.out.println('*'+'\t'+'*');// 93
 		System.out.println('*'+"\t"+'*');// *	*		
 		System.out.println('*'+'\t'+"*");//51*
-		System.out.println('*'+('\t'+"*"));//*	*
+		System.out.println('*'+('\t'+"*"));//* *
+
+		// *****************************
+
+		// String str1=123;// 编译不通过
+		String str1=123+"";
+		System.out.println(str1);//"123"
+		// int num1=str1;
+		// int num1=(int)str1; 编译不通过
+
+		int num1=Integer.parseInt(str1);
+		System.out.println(num1);//123
     }
 
 }
