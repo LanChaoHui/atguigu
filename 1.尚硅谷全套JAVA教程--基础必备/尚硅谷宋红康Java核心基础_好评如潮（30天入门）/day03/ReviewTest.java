b32b45325dacc2aac58598ed9b78aad9d5f00806
胖虎 <1424515472@qq.com>
2021-12-06 02:06:14 +0800

    Create ReviewTest.java
    067.尚硅谷_Java基本语法-复习1
    068.尚硅谷_Java基本语法-复习2：变量的定义
    069.尚硅谷_Java基本语法-复习3：变量的运算规则

---
+++ 1.尚硅谷全套JAVA教程--基础必备/尚硅谷宋红康Java核心基础_好评如潮（30天入门）/day03/ReviewTest.java
@@ -0,0 +1,12 @@
+public class ReviewTest {
+    public static void main(String[] arg) {
+        char c1='a';
+        char c2 =97; // 开发中非常少见
+        System.out.println(c2);
+
+        char c3=5;
+        char c4='5';
+        int i1=c4;
+        System.out.println(i1);
+    }
+}
