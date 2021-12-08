31d0a6b54f9ea5d33382de54068f556933325ab8
胖虎 <1424515472@qq.com>
2021-12-09 02:16:11 +0800

    082.尚硅谷_Java基本语法-位运算符的练习

--- 1.尚硅谷全套JAVA教程--基础必备/尚硅谷宋红康Java核心基础_好评如潮（30天入门）/day03/BitTest.java
+++ 1.尚硅谷全套JAVA教程--基础必备/尚硅谷宋红康Java核心基础_好评如潮（30天入门）/day03/BitTest.java
@@ -5,6 +5,7 @@
 1. 位运算符操作的都是整型的数据
 2. <<：在一定范围内，向左移动一位，相当于*2
      >> : 在一定范围内，向右移动一位，相对于/2;
+面试题：最高校方式的计算2 * 8 ? 2<<3 或 8<<1
 */
 class BitTest 
 {
@@ -14,5 +15,11 @@ class BitTest
 		System.out.println("i<<2="+ (i<< 2));
 		System.out.println("i<<3="+ (i<< 3));
 		System.out.println("i<<3="+ (i<< 27));
+
+		int m=12;
+		int n=5;
+		System.out.println("m & n :"+(m&n));
+		System.out.println("m | n :"+(m|n));
+		System.out.println("m ^ n :"+(m^n));
 	}
 }
