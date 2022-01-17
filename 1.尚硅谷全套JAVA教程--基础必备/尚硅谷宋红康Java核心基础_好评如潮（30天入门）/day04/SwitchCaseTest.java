3e42f5c92209f7619fdc2e25226d8ec7b5acd7b5
胖虎 <1424515472@qq.com>
2022-01-17 12:44:40 +0800

    101.尚硅谷_Java基本语法-switch-case结构的使用说明

--- 1.尚硅谷全套JAVA教程--基础必备/尚硅谷宋红康Java核心基础_好评如潮（30天入门）/day04/SwitchCaseTest.java
+++ 1.尚硅谷全套JAVA教程--基础必备/尚硅谷宋红康Java核心基础_好评如潮（30天入门）/day04/SwitchCaseTest.java
@@ -17,11 +17,18 @@
 ① 根据switch 表达式中的值，依次匹配各个case中的常量。一旦匹配成功，则进入case结构中，调用其执行语句。
       当调用完执行语句以后，仍然继续向下执行其他case结构中的执行语句，直到遇到break关键字，或此switch-case结构末尾为止结束为止。
 ② break,可以使用在switch-case结构中，表示一旦执行到此，就跳出switch-case结构。
+③ switch结构中的表达式，只能是如下的6种数据类型之一：byte、short、char、int、枚举类型(JDK5.0新增)、String类型(JDK7.0新增)
+④ case 之后只能什么常量。不能声明范围.
+⑤ break关键字是可选的。
+⑥ default:相当于if-else中的else，default结构是可选的，而且位置是灵活的。
 */
 class SwitchCaseTest {
     public static void main(String[] args) {
-        int number = 2;
+        int number = 5;
         switch(number) {
+        default:
+            System.out.println("other");
+            break;
         case 0:
             System.out.println("zero");
             break;
@@ -34,9 +41,48 @@ class SwitchCaseTest {
         case 3:
             System.out.println("three");
             break;
+        }
+
+        String season = "summer";
+        switch (season) {
+        case "spring":
+            System.out.println("春暖花开");
+            break;
+        case "summer":
+            System.out.println("夏日炎炎");
+            break;
+        case "autumn":
+            System.out.println("秋高气爽");
+            break;
+        case "winter":
+            System.out.println("冬雪皑皑");
+            break;
         default:
-            System.out.println("other");
+            System.out.println("季节输入有误");
             break;
         }
+
+        //*********************************
+        /*
+        boolean isHandsom=true;
+        switch(isHandsom) {
+        case true:
+            System.out.println("我好帅啊~~~");
+            break;
+        case false:
+            System.out.println("我好丑啊~~~");
+            break;
+        default:
+            System.out.println("输入有误~~~");
+        }*/
+        /*
+        int age=10;
+        switch(age){
+        	case age>18:
+        		System.out.println("成年了");
+        	break;
+        	default:
+        		System.out.println("成年了");
+        }*/
     }
 }
