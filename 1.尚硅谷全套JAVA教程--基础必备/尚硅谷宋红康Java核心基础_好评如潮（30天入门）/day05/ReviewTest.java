5294ce6d362516710e214ec05bc70e86b11a21cd
胖虎 <1424515472@qq.com>
2022-01-19 13:29:19 +0800

    109.尚硅谷_Java基本语法-每天一考

---
+++ 1.尚硅谷全套JAVA教程--基础必备/尚硅谷宋红康Java核心基础_好评如潮（30天入门）/day05/ReviewTest.java
@@ -0,0 +1,14 @@
+class  ReviewTest {
+    public static void main(String[] args) {
+        int sum=0;
+        //for(int i=1; i<100; i++) {
+            if(4%2!=0) {
+               // System.out.println(i);
+                sum=1;
+            }else{
+				sum=2;
+            }
+        //}
+        System.out.println(sum);
+    }
+}
