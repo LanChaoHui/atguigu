5ead089011adeda4f9bd20437aad6089ed9ee57e
胖虎 <1424515472@qq.com>
2022-02-20 20:36:08 +0800

    117.尚硅谷_Java基本语法-嵌套循环的使用2

--- 1.尚硅谷全套JAVA教程--基础必备/尚硅谷宋红康Java核心基础_好评如潮（30天入门）/day05/ForForTest.java
+++ 1.尚硅谷全套JAVA教程--基础必备/尚硅谷宋红康Java核心基础_好评如潮（30天入门）/day05/ForForTest.java
@@ -4,10 +4,11 @@
 	2.
 		外层循环：循环结构B
 		内层循环：循环结构A
-	3. 说明 
+	3. 说明
 	①内层训结构遍历一遍，只相当于外层循环体执行了一次
 	②假设外层循环需要执行m次，内层循环需要执行n次。此时内层循环的循环体一个执行了m*n
-
+	4. 技巧
+		外层循环控制行数，内层循环控制列数
 */
 class ForForTest {
     public static void main(String[] args) {
@@ -16,7 +17,7 @@ class ForForTest {
         for(int i=1; i<=6; i++) {
             System.out.print("*");
         }
-		 System.out.println("\n");
+        System.out.println("\n");
         /*
         	*******
         	*******
@@ -29,5 +30,61 @@ class ForForTest {
             }
             System.out.println();
         }
+
+        /*			 i(行号) j(*的个数)
+        *    1			1
+        **	  2			 2
+        *** 3			 3
+        ****
+        *****
+        ******
+        */
+        for(int i=1; i<=5; i++) { // 行数
+            for(int j=1; j<=i; j++) { // 几个
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        /*
+        ******
+        *****
+        ****
+        ***
+        **
+        *
+        */
+        for(int i=1; i<=6; i++) {
+            for(int j=6; j>=i; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+		/*
+		*    
+        **	  
+        *** 
+        ****
+        *****
+		******
+        *****
+        ****
+        ***
+        **
+        *
+		*/
+		// 略
+		/*
+		    * 
+		   * * 
+		  * * * 
+		 * * * * 
+		* * * * * 
+		 * * * * 
+		  * * * 
+		   * * 
+			 * 
+		*/
+		// 上半部分
+		// 下半部分
     }
 }
