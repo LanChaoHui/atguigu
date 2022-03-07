1f45ca872212fa720aa721222ff43ba4f3d071a5
胖虎 <1424515472@qq.com>
2022-03-07 10:23:02 +0800

    121.尚硅谷_Java基本语法-质数输出的算法优化二

--- 1.尚硅谷全套JAVA教程--基础必备/尚硅谷宋红康Java核心基础_好评如潮（30天入门）/day05/PrimeNumberTest1.java
+++ 1.尚硅谷全套JAVA教程--基础必备/尚硅谷宋红康Java核心基础_好评如潮（30天入门）/day05/PrimeNumberTest1.java
@@ -6,18 +6,22 @@
 class  PrimeNumberTest1 {
     public static void main(String[] args) {
         boolean isFlag=true;// 标识I是否被j除尽，一旦除尽，修改其值
+		int count = 0;// 用来记录质数的个数
         // 获取当前时间距离，1970-01-01 00:00:00 的毫秒数
         long start = System.currentTimeMillis();
         for(int i=2; i<=100000; i++) {// 遍历100000以为的自然数
-            for(int j=2; j<i; j++) {// 被i去除
+			// 优化二：对本身是质数的自然数是有效的。
+			// for(int j=2; j< i; j++) {
+            for(int j=2; j< Math.sqrt(i); j++) {// 被i去除
                 if(i%j==0) { // i 被j除尽
                     isFlag=false;// 优化一：只对本身非质数的自然数有效的
-                    break;
+                   break;
                 }
             }
             // 此时
             if(isFlag) {
-                System.out.println(i);
+               // System.out.println(i);
+			   count++;
             } else {
                 // 重置isFlag
                 isFlag=true;
@@ -25,6 +29,7 @@ class  PrimeNumberTest1 {
         }
         // 获取当前时间距离，1970-01-01 00:00:00 的毫秒数
         long end = System.currentTimeMillis();
-        System.out.println("所花费的时间为："+(end-start));// 21094 - 优化一:break:20216
+		System.out.println("质数的个数为："+count);
+        System.out.println("所花费的时间为："+(end-start));// 11562 - 优化一:break:978 - 优化二：11
     }
 }
