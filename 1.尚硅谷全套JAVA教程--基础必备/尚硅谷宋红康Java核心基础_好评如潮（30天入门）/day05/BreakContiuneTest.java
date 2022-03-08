ac03068665b1c2da4e55483f932a3eb2e135f78d
胖虎 <1424515472@qq.com>
2022-03-09 00:00:20 +0800

    122.尚硅谷_Java基本语法-break和continue的基本使用

--- 1.尚硅谷全套JAVA教程--基础必备/尚硅谷宋红康Java核心基础_好评如潮（30天入门）/day05/BreakContiuneTest.java
+++ 1.尚硅谷全套JAVA教程--基础必备/尚硅谷宋红康Java核心基础_好评如潮（30天入门）/day05/BreakContiuneTest.java
@@ -12,11 +12,22 @@ class BreakContiuneTest
 	{
 		for(int i=1;i<10;i++){
 			if(i%4==0){
-				//break; //123
-				continue;//12356789
+				break; //123
+				//continue;//12356789
 				//System.out.println("²»»áÖ´ÐÐ£¡£¡£¡");
 			}
-			System.out.println(i);
+			System.out.print(i);
+		}
+		System.out.println();
+		// ***********************************************
+		for(int i=1;i<=4;i++){
+			for(int j=1;j<=10;j++){
+				if(j%4==0){
+					break;
+				}
+				System.out.print(i);
+			}
+			System.out.println();
 		}
 	}
 }
