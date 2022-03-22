613be38044108282255f8e7c1d055f87c8252c62
胖虎 <1424515472@qq.com>
2022-03-22 19:08:28 +0800

    129.尚硅谷_Java基本语法-项目一收支明细功能的实现

--- 1.尚硅谷全套JAVA教程--基础必备/尚硅谷宋红康Java核心基础_好评如潮（30天入门）/project1/FamliyAccount.java
+++ 1.尚硅谷全套JAVA教程--基础必备/尚硅谷宋红康Java核心基础_好评如潮（30天入门）/project1/FamliyAccount.java
@@ -1,18 +1,25 @@
 class FamliyAccount {
     public static void main(String[] args) {
         boolean isFlag=true;
+		// 用于记录用户的收入和支出的详情
+		 String details = "收支\t账户金额\t收支金额\t说    明\n";
+        int balance = 10000;
+
         while(isFlag) {
             System.out.println("\n-----------------家庭收支记账软件-----------------\n");
             System.out.println("                   1 收支明细");
             System.out.println("                   2 登记收入");
             System.out.println("                   3 登记支出");
             System.out.println("                   4 退    出\n");
-            System.out.print("                      请选择(1-4)：");
+             System.out.print("                   请选择(1-4)：");
             //  获取用户选择的：1-4
             char selection= Utility.readMenuSelection();
             switch(selection) {
             case '1':
-                System.out.println("1.收支明细");
+                // System.out.println("1.收支明细");
+                System.out.println("-----------------当前收支明细记录-----------------");
+                System.out.println(details);
+                System.out.println("--------------------------------------------------");
                 break;
             case '2':
                 System.out.println("2.登记收入");
@@ -24,9 +31,9 @@ class FamliyAccount {
                 // System.out.println("4.退\n出");
                 System.out.print("确认是否退出(Y/N)：");
                 char isExit=Utility.readConfirmSelection();
-				if(isExit=='Y'){
-					isFlag=false;
-				}
+                if(isExit=='Y') {
+                    isFlag=false;
+                }
                 //break;
             }
         }
