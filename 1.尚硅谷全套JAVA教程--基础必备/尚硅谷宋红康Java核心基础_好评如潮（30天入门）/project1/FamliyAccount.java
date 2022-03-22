4c54c935ae7d090a310f63ed9a969f8d0bc639c1
胖虎 <1424515472@qq.com>
2022-03-22 19:28:22 +0800

    131.尚硅谷_Java基本语法-项目一登记支出功能的实现

--- 1.尚硅谷全套JAVA教程--基础必备/尚硅谷宋红康Java核心基础_好评如潮（30天入门）/project1/FamliyAccount.java
+++ 1.尚硅谷全套JAVA教程--基础必备/尚硅谷宋红康Java核心基础_好评如潮（30天入门）/project1/FamliyAccount.java
@@ -3,7 +3,7 @@ class FamliyAccount {
         boolean isFlag=true;
         // 用于记录用户的收入和支出的详情
         String details = "收支\t账户金额\t收支金额\t说    明\n";
-		// 初始金额
+        // 初始金额
         int balance = 10000;
         while(isFlag) {
             System.out.println("\n-----------------家庭收支记账软件-----------------\n");
@@ -24,17 +24,28 @@ class FamliyAccount {
             case '2':
                 // System.out.println("2.登记收入");
                 System.out.print("本次收入金额：");
-                int money=Utility.readNumber();
+                int addMoney=Utility.readNumber();
                 System.out.print("本次收入说明：");
-                String info=Utility.readString();
-				// 处理balance
-				balance+=money;
-				// 处理details
-				details+=("收入\t"+balance+"\t\t"+money+"\t\t"+info+"\n");
-				 System.out.println("---------------------登记完成---------------------\n");
+                String addInfo=Utility.readString();
+                // 处理balance
+                balance+=addMoney;
+                // 处理details
+                details+=("收入\t"+balance+"\t\t"+addMoney+"\t\t"+addInfo+"\n");
+                System.out.println("---------------------登记完成---------------------\n");
                 break;
             case '3':
-                System.out.println("3.登记支出");
+                // System.out.println("3.登记支出");
+                System.out.print("本次支出金额：");
+                int minuyMoney=Utility.readNumber();
+                System.out.print("本次支出说明：");
+                String  minuyInfo=Utility.readString();
+                // 处理balance
+                if(balance>=minuyMoney) {
+                    balance-=minuyMoney;
+                    details+=("支出\t"+balance+"\t\t"+minuyMoney+"\t\t"+minuyInfo+"\n");
+                } else {
+                    System.out.println("支出超出账户的额度，支付失败");
+                }
                 break;
             case '4':
                 // System.out.println("4.退\n出");
