975d7b6427be87e43cfa02eaab2afff7d6fc7d48
胖虎 <1424515472@qq.com>
2022-03-23 00:33:43 +0800

    137.尚硅谷_Eclipse的使用与数组-Eclipse使用的常见问题2

--- 1.尚硅谷全套JAVA教程--基础必备/尚硅谷宋红康Java核心基础_好评如潮（30天入门）/day05/Contacts/src/com/panhu/contact/HelloWorld.java
+++ 1.尚硅谷全套JAVA教程--基础必备/尚硅谷宋红康Java核心基础_好评如潮（30天入门）/day05/Contacts/src/com/panhu/contact/HelloWorld.java
@@ -1,7 +1,22 @@
 package com.panhu.contact;
 
+/**
+ * 
+ * @Description 这是我的第一个程序
+ * @author LanChaoHui
+ * @version v1.0
+ * @date 2022年3月23日上午12:31:48
+ *
+ */
 public class HelloWorld {
-	public static void main(String[] args) {
-		System.out.println("helloworld!");
+	/**
+	 * @Description
+	 * @author LanChaoHui
+	 * @date 2022年3月23日上午12:32:38
+	 * @param n
+	 * @return
+	 */
+	public int getNumber(int n) {
+		return 0;
 	}
 }
