3f41393ec1a65342dd3fc0d27869e6948125a926
胖虎 <1424515472@qq.com>
2022-03-22 19:19:14 +0800

    130.尚硅谷_Java基本语法-项目二登记收入功能的实现

--- 1.尚硅谷全套JAVA教程--基础必备/尚硅谷宋红康Java核心基础_好评如潮（30天入门）/project1/FamliyAccount.java
+++ 1.尚硅谷全套JAVA教程--基础必备/尚硅谷宋红康Java核心基础_好评如潮（30天入门）/project1/FamliyAccount.java
@@ -1,17 +1,17 @@
 class FamliyAccount {
     public static void main(String[] args) {
         boolean isFlag=true;
-		// 用于记录用户的收入和支出的详情
-		 String details = "收支\t账户金额\t收支金额\t说    明\n";
+        // 用于记录用户的收入和支出的详情
+        String details = "收支\t账户金额\t收支金额\t说    明\n";
+		// 初始金额
         int balance = 10000;
-
         while(isFlag) {
             System.out.println("\n-----------------家庭收支记账软件-----------------\n");
             System.out.println("                   1 收支明细");
             System.out.println("                   2 登记收入");
             System.out.println("                   3 登记支出");
             System.out.println("                   4 退    出\n");
-             System.out.print("                   请选择(1-4)：");
+            System.out.print("                   请选择(1-4)：");
             //  获取用户选择的：1-4
             char selection= Utility.readMenuSelection();
             switch(selection) {
@@ -22,7 +22,16 @@ class FamliyAccount {
                 System.out.println("--------------------------------------------------");
                 break;
             case '2':
-                System.out.println("2.登记收入");
+                // System.out.println("2.登记收入");
+                System.out.print("本次收入金额：");
+                int money=Utility.readNumber();
+                System.out.print("本次收入说明：");
+                String info=Utility.readString();
+				// 处理balance
+				balance+=money;
+				// 处理details
+				details+=("收入\t"+balance+"\t\t"+money+"\t\t"+info+"\n");
+				 System.out.println("---------------------登记完成---------------------\n");
                 break;
             case '3':
                 System.out.println("3.登记支出");
