f96e8c076cf5c4ed9eebc6350777c648497df35e
胖虎 <1424515472@qq.com>
2022-03-30 22:14:29 +0800

    141.尚硅谷_Eclipse的使用与数组-数组的长度与遍历

--- 1.尚硅谷全套JAVA教程--基础必备/尚硅谷宋红康Java核心基础_好评如潮（30天入门）/eclipse-workspace/day06/src/com/panhu/java/ArrayTest.java
+++ 1.尚硅谷全套JAVA教程--基础必备/尚硅谷宋红康Java核心基础_好评如潮（30天入门）/eclipse-workspace/day06/src/com/panhu/java/ArrayTest.java
@@ -35,24 +35,40 @@ public class ArrayTest {
 		int id = 1001;// 声明+初始化
 
 		int[] ids;// 声明
-		//1.1 静态初始化:数组的初始化和数组元素的赋值操作同时进行
+		// 1.1 静态初始化:数组的初始化和数组元素的赋值操作同时进行
 		ids = new int[] { 1001, 1002, 1003, 1004 };
-		//1.2 动态初始化:数组的初始化和数组元素的赋值操作分开进行
-		String[] names=new String[5];
-		
+		// 1.2 动态初始化:数组的初始化和数组元素的赋值操作分开进行
+		String[] names = new String[5];
+
 		// 错误的写法
 //		int[] arr1=new int[];
 //		int[5] arr2=new int[]
 //		int[] arr3=new int[3] {1,2,3}
 		// 总结：数组一旦初始化完成，其长度就确定了。
-		
-		//2. 如何调用数组的指定位置的元素：通过角标的方式调用。
-		//数组的角标（或索引）从0开始，到数组的长度-1结束
-		names[0]="王铭";
-		names[1]="王赫";
-		names[2]="张学良";
-		names[3]="张居龙";
-		names[4]="王宏志";//charAt(0);
+
+		// 2. 如何调用数组的指定位置的元素：通过角标的方式调用。
+		// 数组的角标（或索引）从0开始，到数组的长度-1结束
+		names[0] = "王铭";
+		names[1] = "王赫";
+		names[2] = "张学良";
+		names[3] = "张居龙";
+		names[4] = "王宏志";// charAt(0);
 //		names[5]="周扬";
+
+		// 3. 如何获取数组的长度
+		// 属性:length
+		System.out.println(names.length);// 5
+		System.out.println(ids.length);
+
+		// 4. 如何遍历数组
+		/*
+		 * System.out.println(names[0]); System.out.println(names[1]);
+		 * System.out.println(names[2]); System.out.println(names[3]);
+		 * System.out.println(names[4]);
+		 */
+		for (int i = 0; i < names.length; i++) {
+			System.out.println(names[i]);
+		}
+
 	}
 }
